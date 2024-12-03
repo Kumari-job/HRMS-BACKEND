ff2af662d22c51508f71746cfaf59451f68d4427
Tushar Luitel <tusharluitel88@gmail.com>
2024-12-03 17:04:43 +0545

    feat:updated routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,15 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Router;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        function (Router $router) {
+            $router->middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //

2cb65fe69606d676f698d50587717b177591c955
Tushar Luitel <tusharluitel88@gmail.com>
2024-12-06 10:13:27 +0545

    push

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    dd(now()->startOfDay()->timestamp);
+    return (now()->startOfDay()->timestamp);
 });

802a279cd926e1bb89cd6caa2990bea8e34e4890
Tushar Luitel <tusharluitel88@gmail.com>
2024-12-11 15:05:35 +0545

    added date formatted

--- app/Http/Resources/BranchResource.php
+++ app/Http/Resources/BranchResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Helpers\DateHelper;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -22,7 +23,10 @@ class BranchResource extends JsonResource
             'employee_id' => $this->employee_id,
             'contact_number' => $this->contact_number,
             'established_date' => $this->established_date,
-            'established_at_nepali' => DateHelper::englishToNepali($this->established_date)
+            'established_date_formatted' => Carbon::parse($this->established_date)->format('d M Y'),
+            'established_date_nepali' => DateHelper::englishToNepali($this->established_date,'Y-m-d'),
+            'established_date_nepali_formatted' => DateHelper::englishToNepali($this->established_date),
+
         ];
     }
 }

8e29deb1e601064e6a95860bc3f8c3f92c19026b
Tushar Luitel <tusharluitel88@gmail.com>
2024-12-13 12:29:38 +0545

    feat:added benefit store in contract

--- app/Http/Controllers/Api/EmployeeContractController.php
+++ app/Http/Controllers/Api/EmployeeContractController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\EmployeeBenefitRequest;
 use App\Http\Requests\EmployeeContractRequest;
+use App\Models\EmployeeBenefit;
 use App\Models\EmployeeContract;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,13 +18,17 @@ class EmployeeContractController extends Controller
 
     }
 
-    public function store(EmployeeContractRequest $request)
+    public function store(EmployeeContractRequest $request, EmployeeBenefitRequest $employeeBenefitRequest)
     {
         try {
             $employeeContract =new EmployeeContract($request->all());
             $employeeContract->created_by = Auth::id();
             $employeeContract->save();
-            return response()->json([['success'=>true ,'message'=>'Employee Contract created successfully.']], 201);
+            $employeeBenefit = new EmployeeBenefit($employeeBenefitRequest->all());
+            $employeeBenefit->created_by = Auth::id();
+            $employeeBenefit->save();
+
+            return response()->json([['success'=>true ,'message'=>'Employee contract created successfully.']], 201);
         }catch (\Exception $exception){
             Log::error('Unable to store Employee Contract: '.$exception->getMessage());
             return response()->json(['error'=>true,'message'=>'Unable to create employee contract'],400);

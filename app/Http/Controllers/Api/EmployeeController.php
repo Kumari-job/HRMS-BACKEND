65a335f17af315e34ed19aa0c136ac030bc7c9cc
Tushar Luitel <tusharluitel88@gmail.com>
2024-12-23 10:14:34 +0545

    fix:email validation

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -48,7 +48,7 @@ class EmployeeController extends Controller
     public function store(EmployeeRequest $request)
     {
         $company_id = Auth::user()->selectedCompany->company_id;
-        if (Employee::where('company_id', $company_id)->where('name',$request['name'])->where('email',$request['email'])->exists()) {
+        if (Employee::where('company_id', $company_id)->where('email',$request['email'])->exists()) {
             return response()->json(['error'=>true,"message"=>"Employee already exists"],400);
         }
         $data = $request->except('date_of_birth','image');

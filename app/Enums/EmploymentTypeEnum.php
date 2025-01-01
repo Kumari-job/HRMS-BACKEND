0a06f190a24efde4862344042a8f972be51b4e4c
sarozpradhan64 <sarozpradhan64@gmail.com>
2025-01-01 12:32:13 +0545

    Employment Type enums updated

--- app/Enums/EmploymentTypeEnum.php
+++ app/Enums/EmploymentTypeEnum.php
@@ -4,6 +4,16 @@ namespace App\Enums;
 
 enum EmploymentTypeEnum: string
 {
+
     case Permanent = 'permanent';
-    case Temporary = 'temporary';
+    case PartTime = 'part_time';
+    case FullTime = 'full_time';
+    public function customTitle(): string
+    {
+        return match ($this) {
+            EmploymentTypeEnum::Permanent => 'Permanent',
+            EmploymentTypeEnum::PartTime => 'Part Time Regular',
+            EmploymentTypeEnum::FullTime => 'Full Time Regular',
+        };
+    }
 }

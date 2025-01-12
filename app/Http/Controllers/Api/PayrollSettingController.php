3003ef46c2e972802f65d5b3b28a86b4d840aa4d
Tushar Luitel <tusharluitel88@gmail.com>
2025-01-12 13:17:50 +0545

    feat:completed payroll setting

---
+++ app/Http/Controllers/Api/PayrollSettingController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PayrollSettingRequest;
+use App\Http\Resources\PayrollSettingResource;
+use App\Models\PayrollSetting;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class PayrollSettingController extends Controller
+{
+    public function showPayrollSetting()
+    {
+        $companyProfile = PayrollSetting::first();
+        return new PayrollSettingResource($companyProfile);
+    }
+    public function upsert(PayrollSettingRequest $request)
+    {
+        try{
+            $company_id = Auth::user()->selectedCompany->company_id;
+            $data = $request->validated();
+            $data['created_by'] = Auth::id();
+            $data['updated_by'] = Auth::id();
+            $payrollSetting = new PayrollSetting();
+            $payrollSetting->updateOrCreate(['company_id' => $company_id], $data);
+            return response()->json(['success' => true,'message' => 'Payroll Setting updated successfully.'],200);
+        }catch (\Exception $e){
+            Log::error("Unable to update payroll setting". $e->getMessage());
+            return response()->json(['error' => true,'message' => "Unable to update payroll setting"],500);
+        }
+    }
+}
---
+++ app/Http/Requests/PayrollSettingRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class PayrollSettingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'cit_number' => 'nullable|string',
+            'pf_number' => 'nullable|string',
+            'ssf_number' => 'nullable|string',
+            'bank_name' => 'required|string',
+            'bank_branch_name' => 'required|string',
+            'bank_account_name' => 'required|string',
+            'bank_account_number' => 'required|string',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        if ($this->expectsJson()) {
+            $response = [
+                'error' => true,
+                'errors' => $validator->errors(),
+                'message' => 'There are some issues in the form.'
+            ];
+
+            throw new HttpResponseException(response()->json($response, 422));
+        }
+
+        parent::failedValidation($validator);
+    }
+}

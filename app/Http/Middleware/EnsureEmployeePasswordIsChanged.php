2f52085b133ffab59df230115bae94cd07e3f333
Tushar Luitel <tusharluitel88@gmail.com>
2025-01-19 12:52:33 +0545

    feat:completed employee auth

---
+++ app/Http/Middleware/EnsureEmployeePasswordIsChanged.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureEmployeePasswordIsChanged
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->is_password_changed == 0) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Please change your password.',
+                'url' => url(config('custom.client_app.hrms_app_frontend_url').'/auth/change-password')
+            ], 412);
+        }
+
+        return $next($request);
+    }
+}

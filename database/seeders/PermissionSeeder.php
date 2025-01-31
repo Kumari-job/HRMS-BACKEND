ca0796f5503e560040fd88c0f5fcfac1f14b7ad8
Tushar Luitel <tusharluitel88@gmail.com>
2025-01-31 15:50:47 +0545

    update:added permission seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = ['manage_role','create_employee'];
+        foreach ($permissions as $permission) {
+            Permission::updateOrCreate([
+                'name' => $permission,
+            ],[
+                'guard_name' => 'api',
+            ]);
+        }
+    }
+}

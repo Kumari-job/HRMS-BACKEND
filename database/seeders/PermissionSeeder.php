11e328defaa973cb5128f4927ab1696c170f3858
Tushar Luitel <tusharluitel88@gmail.com>
2025-02-02 11:00:48 +0545

    update:changes in the permission seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class PermissionSeeder extends Seeder
 {
@@ -13,13 +14,31 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        $permissions = ['manage_role','create_employee'];
+        $permissions = ['manage_role', 'create_employee'];
+
+        $role = Role::updateOrCreate(
+            ['name' => 'admin'],
+            ['guard_name' => 'api', 'company_id' => 1]
+        );
+
+        $user = User::where('email', 'administrator@gmail.com')->first();
+        if ($user) {
+            $user->assignRole($role->name);
+        } else {
+            $this->command->warn("Admin user not found. Skipping role assignment.");
+        }
+
         foreach ($permissions as $permission) {
-            Permission::updateOrCreate([
-                'name' => $permission,
-            ],[
-                'guard_name' => 'api',
-            ]);
+            Permission::updateOrCreate(
+                ['name' => $permission],
+                ['guard_name' => 'api']
+            );
+        }
+
+        if ($role) {
+            $role->syncPermissions(Permission::all());
+        } else {
+            $this->command->warn("Admin role not found. Skipping permission assignment.");
         }
     }
 }

ff051dccc725c2bc037daa2292558e809970d3fe
Ayush <ayushwilltry@gmail.com>
2025-02-03 11:08:24 +0545

    ayush

--- app/Http/Controllers/Api/RolePermissionController.php
+++ app/Http/Controllers/Api/RolePermissionController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Helpers\MessageHelper;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -28,16 +30,24 @@ class RolePermissionController extends Controller
 
     public function store(Request $request): JsonResponse
     {
-        $validator = Validator::make($request->all(), [
-            'permission_ids' => 'array',
-            'role_id' => 'required|exists:roles,id',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['error' => true, 'errors' => $validator->errors(), 'message' => MessageHelper::getErrorMessage('form')], 422);
+        try{
+            $validator = Validator::make($request->all(), [
+                'permission_ids' => 'array',
+                'role_id' => 'required|exists:roles,id',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['error' => true, 'errors' => $validator->errors(), 'message' => MessageHelper::getErrorMessage('form')], 422);
+            }
+            $role = Role::findById($request->role_id);
+            $permissions = Permission::whereIn('id', $request->permission_ids)->get();
+            $role->syncPermissions($permissions);
+            return response()->json(['success' => true, 'message' => 'Role permissions assigned successfully.'], 201);
+        
+        }catch(Exception $e)
+        {
+            Log::error("Unable to create: ". $e->getMessage());
+            return response()->json(['error'=>true, 'message'=>'Unable to store role and permission']);
         }
-        $role = Role::findById($request->role_id);
-        $permissions = Permission::whereIn('id', $request->permission_ids)->get();
-        $role->syncPermissions($permissions);
-        return response()->json(['success' => true, 'message' => 'Role permissions assigned successfully.'], 201);
+
     }
 }
--- database/seeders/NotificationSeeder.php
+++ database/seeders/NotificationSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Notifications\Notification;
@@ -15,13 +16,18 @@ class NotificationSeeder extends Seeder
      */
     public function run(): void
     {
+        for($i = 0; $i <= 100; $i++){
+
         DB::table('notifications')->insert([
             'id' => Str::uuid(),
             'type' => 'App\Notifications\LeaveNotification',
             'data' =>'{"title":"Employee Leave","description":"Tushar Luitelllll has requested Paternity on 2025-01-21"}',
             'notifiable_type' => 'App\Models\User',
             'notifiable_id' => 1,
-
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        
         ]);
+    }
     }
 }
